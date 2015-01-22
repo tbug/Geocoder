4c094818cedac427b6a6bccc55da5115276d7054
Andreas Kristiansen <ak@bownty.com>
2015-01-22 13:18:13 +0000

    Fix entire file double indented

--- src/Geocoder/Provider/MapBoxProvider.php
+++ src/Geocoder/Provider/MapBoxProvider.php
@@ -12,99 +12,101 @@ use Geocoder\Exception\UnsupportedException;
  */
 class MapBoxProvider extends AbstractProvider implements ProviderInterface
 {
-		/**
-		 * @const string
-		 */
-		const GEOCODE_ENDPOINT_URL = 'http://api.tiles.mapbox.com/v4/geocode/mapbox.places/%s.json?access_token=%s';
+	/**
+	 * @const string
+	 */
+	const GEOCODE_ENDPOINT_URL = 'http://api.tiles.mapbox.com/v4/geocode/mapbox.places/%s.json?access_token=%s';
 
-		/**
-		 * API Access Token
-		 *
-		 * @var string
-		 */
-		protected $apiKey = null;
+	/**
+	 * API Access Token
+	 *
+	 * @var string
+	 */
+	protected $apiKey = null;
 
-		/**
-		 * Constructor
-		 *
-		 * @param HttpAdapterInterface $adapter An HTTP adapter.
-		 * @param string $apiKey An API key.
-		 * @param string|null $locale A locale (optional).
-		 * @return void
-		 */
-		public function __construct(HttpAdapterInterface $adapter, $apiKey, $locale = null)
-		{
-			parent::__construct($adapter, $locale);
-			$this->apiKey = $apiKey;
+	/**
+	 * Constructor
+	 *
+	 * @param HttpAdapterInterface $adapter An HTTP adapter.
+	 * @param string $apiKey An API key.
+	 * @param string|null $locale A locale (optional).
+	 * @return void
+	 */
+	public function __construct(HttpAdapterInterface $adapter, $apiKey, $locale = null)
+	{
+		parent::__construct($adapter, $locale);
+		$this->apiKey = $apiKey;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getGeocodedData($address)
+	{
+		if (!$this->apiKey) {
+			throw new InvalidCredentialsException('No API Key provided.');
 		}
 
-		/**
-		 * {@inheritDoc}
-		 */
-		public function getGeocodedData($address)
-		{
-			if (!$this->apiKey) {
-				throw new InvalidCredentialsException('No API Key provided.');
-			}
+		$query = sprintf(self::GEOCODE_ENDPOINT_URL, urlencode($address), $this->apiKey);
 
-			$query = sprintf(self::GEOCODE_ENDPOINT_URL, urlencode($address), $this->apiKey);
+		return $this->executeQuery($query);
+	}
 
-			return $this->executeQuery($query);
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getReversedData(array $coordinates)
+	{
+		throw new \BadMethodCallException('MapBoxProvider does not yet support reversed lookups');
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getName()
+	{
+		return 'map_box';
+	}
+
+	/**
+	 * Query a constructed end point
+	 *
+	 * @param string $query
+	 * @return array
+	 */
+	protected function executeQuery($query)
+	{
+		$content = $this->getAdapter()->getContent($query);
+
+		if (!$content) {
+			throw new NoResultException(sprintf('Could not execute query: %s', $query));
 		}
 
-		/**
-		 * {@inheritDoc}
-		 */
-		public function getReversedData(array $coordinates)
-		{
-			throw new \BadMethodCallException('MapBoxProvider does not yet support reversed lookups');
+		$json = json_decode($content, true);
+
+		if (empty($json['features'])) {
+			throw new NoResultException(sprintf('Could not find results for given query: %s', $query));
 		}
 
-		/**
-		 * {@inheritDoc}
-		 */
-		public function getName()
-		{
-			return 'map_box';
+		$results = [];
+
+		foreach ($json['features'] as $location) {
+			if ($location['relevance'] < .75) {
+				continue;
+			}
+
+			$results = array_merge($this->getDefaults(), [
+				'latitude' => $location['center'][1],
+				'longitude' => $location['center'][0],
+				'city' => $location['text']
+			]);
 		}
 
-		/**
-		 * @param string $query
-		 *
-		 * @return array
-		 */
-		protected function executeQuery($query)
-		{
-			$content = $this->getAdapter()->getContent($query);
-
-			if (!$content) {
-				throw new NoResultException(sprintf('Could not execute query: %s', $query));
-			}
-
-			$json = json_decode($content, true);
-
-			if (empty($json['features'])) {
-				throw new NoResultException(sprintf('Could not find results for given query: %s', $query));
-			}
-
-			$results = [];
-
-			foreach ($json['features'] as $location) {
-				if ($location['relevance'] < .75) {
-					continue;
-				}
-
-				$results = array_merge($this->getDefaults(), [
-					'latitude' => $location['center'][1],
-					'longitude' => $location['center'][0],
-					'city' => $location['text']
-				]);
-			}
-
-			if (empty($results)) {
-				throw new NoResultException(sprintf('Could not find results for given query: %s', $query));
-			}
-
-			return $results;
+		if (empty($results)) {
+			throw new NoResultException(sprintf('Could not find results for given query: %s', $query));
 		}
+
+		return $results;
+	}
+
 }
