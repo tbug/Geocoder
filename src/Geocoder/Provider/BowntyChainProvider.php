6bf3859c0f45a1a428511693aff73d2690c07775
Andreas Kristiansen <ak@bownty.com>
2015-01-23 10:52:07 +0000

    Fix indentation

--- src/Geocoder/Provider/BowntyChainProvider.php
+++ src/Geocoder/Provider/BowntyChainProvider.php
@@ -12,94 +12,90 @@ use Geocoder\Exception\ChainNoResultException;
  *
  * @author Andreas Kristiansen <mail@ankr.dk>
  */
-class BowntyChainProvider extends ChainProvider implements ProviderInterface {
+class BowntyChainProvider implements ProviderInterface {
 
-  /**
-   * @var ProviderInterface[]
-   */
-  private $providers = array();
+    /**
+     * @var ProviderInterface[]
+     */
+    private $providers = array();
 
-  /**
-   * Constructor
-   *
-   * @param ProviderInterface[] $providers
-   */
-  public function __construct(array $providers = array())
-  {
-	$this->providers = $providers;
-  }
+    /**
+     * Constructor
+     *
+     * @param ProviderInterface[] $providers
+     */
+    public function __construct(array $providers = array())
+    {
+        $this->providers = $providers;
+    }
 
-  /**
-   * Add a provider
-   *
-   * @param ProviderInterface $provider
-   */
-  public function addProvider(ProviderInterface $provider)
-  {
-	$this->providers[] = $provider;
-  }
+    /**
+     * Add a provider
+     *
+     * @param ProviderInterface $provider
+     */
+    public function addProvider(ProviderInterface $provider)
+    {
+        $this->providers[] = $provider;
+    }
 
-  /**
-   * {@inheritDoc}
-   */
-  public function getGeocodedData($address)
-  {
-	$exceptions = array();
+    /**
+     * {@inheritDoc}
+     */
+    public function getGeocodedData($address)
+    {
+        $exceptions = array();
+        foreach ($this->providers as $provider) {
+            try {
+                $results = $provider->getGeocodedData($address);
+                foreach ($results as &$result) {
+                    $result['__provider_name'] = $provider->getName();
+                }
+                return $results;
+            } catch (InvalidCredentialsException $e) {
+                throw $e;
+            } catch (\Exception $e) {
+                $exceptions[] = $e;
+            }
+        }
+        throw new ChainNoResultException(sprintf('No provider could provide the address "%s"', $address), $exceptions);
+    }
 
-	foreach ($this->providers as $provider) {
-	  try {
-		$results = $provider->getGeocodedData($address);
-		foreach ($results as &$result) {
-			$result['__provider_name'] = $provider->getName();
-		}
-		return $results;
-	  } catch (InvalidCredentialsException $e) {
-		throw $e;
-	  } catch (\Exception $e) {
-		$exceptions[] = $e;
-	  }
-	}
+    /**
+     * {@inheritDoc}
+     */
+    public function getReversedData(array $coordinates)
+    {
+        $exceptions = array();
+        foreach ($this->providers as $provider) {
+            try {
+                return $provider->getReversedData($coordinates);
+            } catch (InvalidCredentialsException $e) {
+                throw $e;
+            } catch (\Exception $e) {
+                $exceptions[] = $e;
+            }
+        }
+        throw new ChainNoResultException(sprintf('No provider could provide the coordinated %s', json_encode($coordinates)), $exceptions);
+    }
 
-	throw new ChainNoResultException(sprintf('No provider could provide the address "%s"', $address), $exceptions);
-  }
+    /**
+     * {@inheritDoc}
+     */
+    public function setMaxResults($limit)
+    {
+        foreach ($this->providers as $provider) {
+            $provider->setMaxResults($limit);
+        }
+        return $this;
+    }
 
-  /**
-   * {@inheritDoc}
-   */
-  public function getReversedData(array $coordinates)
-  {
-	$exceptions = array();
+    /**
+     * {@inheritDoc}
+     */
+    public function getName()
+    {
+        return 'bownty_chain';
+    }
 
-	foreach ($this->providers as $provider) {
-	  try {
-		return $provider->getReversedData($coordinates);
-	  } catch (InvalidCredentialsException $e) {
-		throw $e;
-	  } catch (\Exception $e) {
-		$exceptions[] = $e;
-	  }
-	}
-
-	throw new ChainNoResultException(sprintf('No provider could provide the coordinated %s', json_encode($coordinates)), $exceptions);
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  public function setMaxResults($limit)
-  {
-	foreach ($this->providers as $provider) {
-	  $provider->setMaxResults($limit);
-	}
-
-	return $this;
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  public function getName()
-  {
-	return 'chain';
-  }
 }
